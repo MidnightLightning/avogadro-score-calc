b3b519fab8611a7971d17642592b57d130873589
Derek Schindhelm <ddschindhelm@gmail.com>
2013-11-21 14:35:55 -0600

    Now computes raw score from algorithm.

--- js/Derek.js
+++ js/Derek.js
@@ -1,5 +1,5 @@
 $(function(){
-	$(".var-entry").change(computeRawScoresHardCoded);
+	$(".var-entry").change(computeRawScores);
 });
 var computeRawScoresHardCoded = function(){
 	//Loop through every row in the table.
@@ -25,6 +25,39 @@ var computeRawScoresHardCoded = function(){
 					if(rowLoadSupported != "" && rowMass != "" && rowLineClearance != ""){
 						var result = (rowLoadSupported/rowMass) + (rowLineClearance * .1);
 						$(this).find("input").val(result);
+					}
+				}
+			});
+		}
+	});
+};
+
+var computeRawScores = function(){
+	//Loop through every row in the table.
+	$("#event_raw_table").find("tr").each(function(rowIndex){
+		//Ignore first and second row, which has the header info. Index starts with first row being 0.
+		if(rowIndex > 1 && $(this).find("td").length > 5){
+			for(var i = 0; i < avgo.forumulaVariables.length; i++){
+				window[avgo.forumulaVariables[i]] = "";
+			}
+			var totalColumns = $(this).find("td").length;
+			//Loop through each column of the current row.
+			$(this).find("td").each(function(columnIndex){
+				if(columnIndex > 2){
+					if(columnIndex < totalColumns - 2){
+						window[avgo.forumulaVariables[columnIndex - 3]] = $(this).find("input").val();
+					}else if (columnIndex == totalColumns - 2){
+						var canSet = true;
+						for(var i = 0; i < avgo.forumulaVariables.length; i++){
+							if(window[avgo.forumulaVariables[i]] == ""){
+								canSet = false;
+							}
+						}
+						if(canSet){
+							var result = eval(avgo.eventFormula);
+							$(this).find("input").val(result);
+						}
+						
 					}
 				}
 			});
