f25f925b05132c94eebf3521af115089c15f1adb
ExaByTEz <vangd0213@gmail.com>
2013-11-23 20:46:11 -0600

    Update var_entry_validation.js
    Removed input restriction, Firefox compatibility issues
    Fixed indentation issues of code
    Changed how the max values are determined
    Added feature to detect if '-' is the first character
    Added feature to update decimal value to max if beyond max
    Added feature to turn cell pink if input is not a number
    Misc changes/bug fixes.

--- js/var_entry_validation.js
+++ js/var_entry_validation.js
@@ -1,70 +1,83 @@
 /**
  * @author Dean Vang
  */
- 
-$(function(){ //function used to restrict character type in var-entry classes
-  $('.var-entry').keypress(function(e){ //when a key is pressed inside a object with the class 'var-entry' perform the following function
-  if(e.keyCode == 45 || e.keyCode == 46 || e.keyCode >= 48 && e.keyCode <= 57){}//'.' or '-' or '0-9'
-  else if(e.keyCode >= 96 && e.keycode <= 105){}//0-9  numpad
-  else{
-    return false;	
-    }
-  });
-  
-  $('.var-entry').keyup(function(e){
-    js_validate_var_entry();
-  });
+
+$(function(){
+	$('.var-entry').keyup(function(e){
+		js_validate_var_entry();
+	});
 });
 
-function js_validate_var_entry(){ //todo:check for precision > 9 decimal places
-  $('.var-entry').each(function(){ //for each class 'var-entry', do the following function
-    var fields = $(this).find(':text'); //find all text inputs
-    var var_val = fields.eq(0).val();
-    var var_valid = js_validate(var_val);
-    
-    if(var_valid){
-       fields.eq(0).css('background-color', 'white'); 
-    }else{
-       fields.eq(0).css('background-color', 'pink'); 
-    }
-    
-    if(var_val > 999999999.999999999){ //set to max if above max, does not check precision
-       alert('value too high for ' + fields.eq(0).attr('name')); //for debugging
-      if(var_val.split('.').length < 2){ //no decimal places are in the number
-        fields.eq(0).val('999999999');
-      }
-      else{ //the number contains a decimal place
-        fields.eq(0).val('999999999' + '.' + var_val.split('.')[1]); //attach decimal to max number
-      }
-    }else if(var_val < -999999999.999999999){ //set to min if below min, does not check precision
-       alert('value too low for ' + fields.eq(0).attr('name')); //for debugging
-       if(var_val.split('.').length < 2){ //no decimal places are in the number
-        fields.eq(0).val('-999999999');
-      }
-      else{ //the number contains a decimal place
-        fields.eq(0).val('-999999999' + '.' + var_val.split('.')[1]); //attach decimal to max number
-      }   
-    }
-  });
+function js_validate_var_entry(){
+	$('.var-entry').each(function(){ //for each class 'var-entry', do the following function
+		var fields = $(this).find(':text'); //find all text inputs
+		var var_val = fields.eq(0).val(); //find the value
+		var var_num = var_val.split('.'); //splits number where '.' is present
+		var var_valid = js_validate(var_val); //checks if number is valid decimal or a valid negative number
+		var num_overflow = false; //flag indicating a number needs to be updated to the max
+		var max_val = 999999999; //maximum supported value
+
+		if(var_valid){
+			fields.eq(0).css('background-color', 'white'); 
+		}else{
+			fields.eq(0).css('background-color', 'pink'); 
+		}
+
+		if(var_num.length > 0){ //if there is a number present
+			if(var_num[0] > max_val){ //if left side > max
+				num_overflow = true; //indicate update needed
+				var_num[0] = max_val; //update left side to max
+			}
+			if(var_num[0] < -max_val){
+				num_overflow = true; //indicate update needed
+				var_num[0] = -max_val; //update left side to min
+			}
+			if(var_num.length > 1 && Math.abs(var_num[1]) > max_val){
+				num_overflow = true; //indicate update needed
+				var_num[1] = max_val; //update right side to max
+			}
+			if(num_overflow){ //if update is needed
+				if(var_num.length > 1){ //if there is a decimal in the number
+					var_val = var_num[0] + '.' + var_num[1]; //combine the number    
+				}else{ 
+					var_val = var_num[0]; //update integer value
+				}
+				fields.eq(0).val(var_val); //update value 
+			} 
+		}
+	});
 }
 
-function js_validate_dec(num){
-  var num_arr = num.split('.');
-  if(num_arr.length > 1 && num_arr[1].split('-').length > 1){
-     return false; // user types '.-'
-  } 
-  return (num_arr.length > 2 ? false : true); //more than 1 '.' or 9 decmial places
+function js_validate(num){ //runs validation 
+	if(js_validate_dec(num) && js_validate_neg(num)){
+		return true; 
+	}else{
+		return false; 
+	}
 }
 
-function js_validate(num){
-  if(js_validate_dec(num) && js_validate_neg(num)){
-     return true; 
-  }else{
-     return false; 
-  }
+function js_validate_dec(num){ //basic check if a number is valid 
+	var num_arr = num.split('.');
+	if(num_arr.length > 2){ //more than 1 decimal
+		return false;   
+	}
+	if(num_arr.length > 1){
+		if(num_arr[1].match(/[^0-9]/)){ //right does not contain '0~9'        
+			return false;
+		}
+	}
+	if(num_arr.length > 0){
+		if(num_arr[0].match(/[^\-0-9]/)){ //left does not contain '-' or '0~9'
+			return false;     
+		}
+	}
+	if(num.split('-').length > 1 && num.indexOf('-') !== 0){
+		return false; //'-' is not the first character
+	}
+	return true; //valid otherwise
 }
 
 function js_validate_neg(num){
-  var num_arr = num.split('-');
-  return (num_arr.length > 2 ? false : true);
+	var num_arr = num.split('-');
+	return (num_arr.length > 2 ? false : true);
 }
