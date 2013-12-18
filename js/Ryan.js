1b0764964ec5a366c21182c06fc8aaf70e7c0160
felrya <felrya92@live.com>
2013-12-17 23:06:37 -0600

    Made disabling more dynamic

--- js/Ryan.js
+++ js/Ryan.js
@@ -4,37 +4,43 @@ $(function(){
 });
 
 var rawBoxDisabler = function(){
+	var disableFlag = false;
+	var count = 0;
+	var allFull = false;
 	//Loop through every row in the table.
 	$("#event_raw_table").find("tr").each(function(rowIndex){
-		//Ignore first and second row, which has the header info. Index starts with first row being 0.
+		//Ignore first and second row, which has the header info.		
 		if(rowIndex > 1 && $(this).find("td").length > 5){
-			var rowLoadSupported = "";
-			var rowMass = "";
-			var rowLineClearance = "";
+			var numColumns = $(this).find("td").length;
+			var variableValue = "";
+			var numVariables = $(this).find("td").length - 5;
 			//Loop through each column of the current row.
 			$(this).find("td").each(function(columnIndex){
-				if(columnIndex == 3){
-					//Finds the input child of the current colmun and sets the rowLoadSupported variable to be the value of the input.
-					rowLoadSupported = $(this).find("input").val(); 
-					//With jQuery, if nothing is in the "()" then it is a "getter".
-					//If there is a value in the "()", then it is a "setter".
-				}else if(columnIndex == 4){
-					rowMass = $(this).find("input").val();
-				}else if(columnIndex == 5){
-					rowLineClearance = $(this).find("input").val();
-				}else if(columnIndex == 6){
-					//Check to make sure all fields have values, then compute score and set score box.
-					if(rowLoadSupported != "" || rowMass != "" || rowLineClearance != ""){
-						$(this).find("input").attr("disabled", true);
-						if(rowLoadSupported != "" && rowMass != "" && rowLineClearance != ""){
-						} else{
-							$(this).find("input").val("");	
+				//Check for values in var-entry textboxes
+				if (columnIndex >= 3 && columnIndex < numVariables + 3){
+					variableValue = $(this).find("input").val();
+					if (variableValue != ""){
+						//A var-entry textbox has a value in it
+						disableFlag = true;
+						count++;
+						if (count == numVariables){
+							allFull = true;	
 						}
-						//$(this).find("input").val("");
 					}
-					else
-					{
+				}
+				//Disable raw-score textbox if disableFlag is true
+				if (columnIndex == numVariables + 3){
+					if(disableFlag){
+						$(this).find("input").attr("disabled", true);
+					}
+					else{
 						$(this).find("input").attr("disabled", false);	
+					}
+					disableFlag = false;
+					count = 0;
+					//Clear raw-score box if not all var-entry boxes are filled
+					if (!allFull){
+						$(this).find("input").val("");	
 					}
 				}
 			});
@@ -43,14 +49,20 @@ var rawBoxDisabler = function(){
 };
 
 var varBoxDisabler = function(){
+	//Loop through each row of the table
 	$("#event_raw_table").find("tr").each(function(rowIndex){
+		//Ingore first two rows of table which contains the header info
 		if(rowIndex > 1 && $(this).find("td").length > 5){
 			var rawScore = "NULL";
+			var numVariables = $(this).find("td").length - 5;
+			//Loop through columns of table in reverse order
 			$($($(this).find("td")).get().reverse()).each(function(columnIndex){
+				//Get value of raw-score box if it contains something and is not disabled
 				if(columnIndex == 1 && $(this).find("input").val() != "" && !$(this).find("input").is(":disabled")){
 					rawScore = $(this).find("input").val();	
 				}
-				if(columnIndex >= 2 && columnIndex <= 4){
+				//Disable var-entry boxes if needed
+				if(columnIndex >= 2 && columnIndex <= numVariables + 1){
 					if (rawScore != "NULL"){
 						$(this).find("input").attr("disabled", true);	
 					} else{
